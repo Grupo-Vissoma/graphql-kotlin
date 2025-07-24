3866626994edcf2656f7a9f6e5aea3c8edff978b
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-07-24 13:01:27 +0100

    publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import java.net.URI
 import java.time.Duration
 
 description = "Libraries for running a GraphQL server in Kotlin"
@@ -5,6 +6,7 @@ extra["isReleaseVersion"] = !version.toString().endsWith("SNAPSHOT")
 
 plugins {
     alias(libs.plugins.nexus.publish)
+    id("maven-publish")
 }
 
 allprojects {
@@ -50,6 +52,19 @@ tasks {
         // we will need to run them after artifacts are published to local m2 repo
         for (graphQLKotlinProject in project.childProjects) {
             dependsOn(":${graphQLKotlinProject.key}:publishToMavenLocal")
+        }
+    }
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI.create("https://maven.pkg.github.com/grupo-vissoma/graphql-kotlin")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
         }
     }
 }
